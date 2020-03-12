159945943270f715109d6f01490834a512b8f8af
AnastasiyaTiu <stacyti8888@gmail.com>
2020-03-11 22:33:26 -0700

    BrowserFactory

--- src/test/java/com/cbt/utilities/BrowserFactory.java
+++ src/test/java/com/cbt/utilities/BrowserFactory.java
@@ -1,7 +1,28 @@
 package com.cbt.utilities;
 
-public class BrowserFactory {
-    public static void main(String[] args) {
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
+public class BrowserFactory {
+
+    public static WebDriver getDriver(String browser) {
+        if (browser.equalsIgnoreCase("safari")) {
+            return null;
+        } else if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().version("79").setup();
+            return new ChromeDriver();
+        }else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        }else if (browser.equalsIgnoreCase("edge")) {
+            return new EdgeDriver();
+        }else{
+            WebDriverManager.operadriver().setup();
+                return new OperaDriver();
+        }
     }
 }
